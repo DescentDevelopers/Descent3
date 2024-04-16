7d2e1b5cba2f0bb400174dce42d24d34f644de99
Thomas Otto <th1000s@posteo.net>
2024-04-16 22:31:48 +0200

    gcc: disable `-Wnarrowing` via pragma in snd8to16.h

--- libmve/snd8to16.h
+++ libmve/snd8to16.h
@@ -14,6 +14,7 @@
 */
 #pragma warning(disable : 4245)
 #pragma warning(disable : 4309)
+#pragma GCC diagnostic ignored "-Wnarrowing"
 
 signed short snd_8to16[256] = {
     0,      1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,
@@ -35,5 +36,7 @@ signed short snd_8to16[256] = {
     -32,    -31,    -30,    -29,    -28,    -27,    -26,    -25,    -24,    -23,    -22,    -21,    -20,    -19,
     -18,    -17,    -16,    -15,    -14,    -13,    -12,    -11,    -10,    -9,     -8,     -7,     -6,     -5,
     -4,     -3,     -2,     -1};
+
+#pragma GCC diagnostic pop
 #pragma warning(default : 4245)
 #pragma warning(default : 4309)
