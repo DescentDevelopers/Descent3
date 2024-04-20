31b63bb79f9310785b5fb1673918db3127fd4c02
Louis Gombert <louisgombert@gmail.com>
2024-04-20 18:23:08 +0200

    Add license to legacy utilities
    Generated using script:
    for file in \$(find ./legacy -type f | \grep -E "\.cpp|\.h")
    do
      mv $file temp.xxx
      cp gplheader $file
      cat temp.xxx >> $file
    done

--- legacy/editor/ObjectListCombo.h
+++ legacy/editor/ObjectListCombo.h
@@ -1,3 +1,21 @@
+/*
+* Descent 3 
+* Copyright (C) 2024 Parallax Software
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #if !defined(AFX_OBJECTLISTCOMBO_H__C8C81360_C67E_11D2_A13A_0060089A83BE__INCLUDED_)
 #define AFX_OBJECTLISTCOMBO_H__C8C81360_C67E_11D2_A13A_0060089A83BE__INCLUDED_
 
--- legacy/musicutils/RawCvtDlg.h
+++ legacy/musicutils/RawCvtDlg.h
@@ -1,3 +1,21 @@
+/*
+* Descent 3 
+* Copyright (C) 2024 Parallax Software
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #if !defined(AFX_RAWCVTDLG_H__D94CC63D_1CA3_11D2_AF2D_0060089A8025__INCLUDED_)
 #define AFX_RAWCVTDLG_H__D94CC63D_1CA3_11D2_AF2D_0060089A8025__INCLUDED_
 
