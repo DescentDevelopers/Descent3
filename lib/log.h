b0e5754e96fdb92dffdb0112730db97214ac34b0
Bryan Perris <7431407+bryanperris@users.noreply.github.com>
2024-04-28 18:10:32 -0400

    Introduce spdlog logging system
    * The game version is now printed via the log system rather than from within the loki_initialize function.
    remove this line

---
+++ lib/log.h
@@ -0,0 +1,28 @@
+/*
+* Descent 3
+* Copyright (C) 2024 Descent Developers
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_sinks.h"
+
+#ifndef LOG_H
+#define LOG_H
+
+// Initialize the logging system
+void InitLog();
+
+#endif // LOG_H
