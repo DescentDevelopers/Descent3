b12baa97156af1bb95f4386eff3cb6fcd7ed042c
ziplantil <magnoboost@gmail.com>
2024-05-08 20:30:39 +0300

    add one more test; this feeds 60 bytes and tests 'overflow' in blocks

--- tests/md5_tests.cpp
+++ tests/md5_tests.cpp
@@ -51,6 +51,7 @@ TEST(D3, MD5) {
   EXPECT_EQ(hexdigest_after_n_rounds_of_fuzz(0), "d41d8cd98f00b204e9800998ecf8427e");
   EXPECT_EQ(hexdigest_after_n_rounds_of_fuzz(1), "c56c005504f5b9b17df9d83f1106e9b2");
   EXPECT_EQ(hexdigest_after_n_rounds_of_fuzz(2), "98327a3f4ee311c9bdc8613508922c95");
+  EXPECT_EQ(hexdigest_after_n_rounds_of_fuzz(4), "f2343ce526646c087df92d708e883675");
   EXPECT_EQ(hexdigest_after_n_rounds_of_fuzz(100), "99b30c1dcc42d97c3d914c26a14bb1d7");
   EXPECT_EQ(hexdigest_after_n_rounds_of_fuzz(1999), "82eeff8c7d574c8232b0ca6ca2c9dd40");
 }
