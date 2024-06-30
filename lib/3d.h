6684072c6e25a3f5ab07cd48ed1369b1e15c03bb
Chris Sarbora <tophyr@gmail.com>
2024-06-30 02:46:52 -0500

    dead code

--- lib/3d.h
+++ lib/3d.h
@@ -430,7 +430,6 @@ void g3_DrawSpecialLine(g3Point *p0, g3Point *p1);
 // Draws a bitmap on a specific plane.  Also does rotation.  Angle of rotation is passed as 'rot_angle'
 void g3_DrawPlanarRotatedBitmap(vector *pos, vector *norm, angle rot_angle, float width, float height, int bm);
 
-void g3_TransformVert(float res[4], float pt[4], float a[4][4]);
 void g3_TransformMult(float res[4][4], float a[4][4], float b[4][4]);
 void g3_TransformTrans(float res[4][4], float t[4][4]);
 void g3_GetModelViewMatrix(const vector *viewPos, const matrix *viewMatrix, float *mvMat);
