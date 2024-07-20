d61deae68dafafccbff8540734e171acf7486012
Chris Sarbora <tophyr@gmail.com>
2024-07-20 16:19:31 -0500

    add rudimentary shaders and machinery

---
+++ renderer/shaders/vertex.glsl
@@ -0,0 +1,29 @@
+#version 150 core
+
+/*
+* Descent 3
+* Copyright (C) 2024 Descent Developers
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+in vec3 in_pos;
+in vec4 in_color;
+in vec2 in_uv0;
+in vec2 in_uv1;
+
+void main()
+{
+    gl_Position = vec4(0);
+}
