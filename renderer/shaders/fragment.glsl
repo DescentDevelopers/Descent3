5b4703bf5fc33476ae509dda273dc44272d91169
Chris Sarbora <tophyr@gmail.com>
2024-07-28 22:26:12 -0500

    pipe color and texcoords thru the shaders

--- renderer/shaders/fragment.glsl
+++ renderer/shaders/fragment.glsl
@@ -18,9 +18,16 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+in vec4 vertex_color;
+in vec2 vertex_uv0;
+in vec2 vertex_uv1;
+
 out vec4 out_color;
+
+uniform sampler2D u_texture0;
+uniform sampler2D u_texture1;
 
 void main()
 {
-    out_color = vec4(0);
+    out_color = vertex_color * texture(u_texture0, vertex_uv0) * texture(u_texture1, vertex_uv1);
 }
