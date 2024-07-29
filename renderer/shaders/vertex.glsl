a9e5aa07be86ba47ffe151c3b3ee7fce04e11204
Chris Sarbora <tophyr@gmail.com>
2024-07-28 20:00:19 -0500

    add VertexBuffer and VertexAttribs

--- renderer/shaders/vertex.glsl
+++ renderer/shaders/vertex.glsl
@@ -1,6 +1,9 @@
 #version 330 core
 
-layout (location = 0) in vec3 in_position;
+layout (location = 0) in vec3 in_pos;
+layout (location = 1) in vec4 in_color;
+layout (location = 2) in vec2 in_uv0;
+layout (location = 3) in vec2 in_uv1;
 
 void main()
 {
