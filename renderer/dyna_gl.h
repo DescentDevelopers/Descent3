52277a317408acc11742a2488693345304f09229
Chris Sarbora <tophyr@gmail.com>
2024-07-31 23:38:57 -0500

    eliminate hints to things the shader does now

--- renderer/dyna_gl.h
+++ renderer/dyna_gl.h
@@ -189,7 +189,6 @@ DYNAEXTERN(glGetShaderiv);
 DYNAEXTERN(glGetString);
 DYNAEXTERN(glGetStringi);
 DYNAEXTERN(glGetUniformLocation);
-DYNAEXTERN(glHint);
 DYNAEXTERN(glLinkProgram);
 DYNAEXTERN(glPixelStorei);
 DYNAEXTERN(glPixelTransferi);
