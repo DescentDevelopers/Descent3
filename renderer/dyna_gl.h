e455ed5fa58615b4d39c9eefae74a85295b5a434
Chris Sarbora <tophyr@gmail.com>
2024-08-14 09:34:13 -0500

    Gate OpenGL error-checking on a compilation setting rather than Debug/Release

--- renderer/dyna_gl.h
+++ renderer/dyna_gl.h
@@ -39,7 +39,7 @@
 #endif
 
 inline void CheckError() {
-#if defined(_DEBUG)
+#if defined(FATAL_GL_ERRORS)
   static auto _dglGetError = reinterpret_cast<GLenum (*)()>(SDL_GL_GetProcAddress("glGetError"));
   auto err = _dglGetError();
   switch (err) {
