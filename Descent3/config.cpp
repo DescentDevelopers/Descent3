DescentDevelopers/Descent3

commit 40c7f0dbed60aca584302aaff0b31732bf8c4c4f

Add higher resolutions to the settings
Create a new menu to allow the user to choose higher resolutions

Descent3/config.cpp
@@ -304,13 +304,38 @@
 #include "ctlconfig.h"
 #include "d3music.h"
 
+#include <algorithm>
+#include <vector>
+
 #define STAT_SCORE STAT_TIMER
 
-tVideoResolution Video_res_list[N_SUPPORTED_VIDRES] = {
-    {512, 384}, {640, 480}, {800, 600}, {960, 720}, {1024, 768}, {1280, 960}, {1600, 1200}, {640, 480} // custom
-};
+std::vector<tVideoResolution> Video_res_list = {{512, 384},
+                                                {640, 480},
+                                                {800, 600},
+                                                {960, 720},
+                                                {1024, 768},
+                                                {1280, 960},
+                                                {1600, 1200},
+                                                // 16:9
+                                                {1280, 720},
+                                                {1366, 768},
+                                                {1368, 768},
+                                                {1680, 1050},
+                                                {1920, 1080},
+                                                {2560, 1440},
+                                                {3840, 2160},
+                                                // 16:10
+                                                {1280, 800},
+                                                {1920, 1200},
+                                                {2560, 1600},
+                                                // Ultrawide
+                                                {2560, 1080},
+                                                {2880, 1200},
+                                                {3440, 1440},
+                                                {3840, 1600}};
 
-int Game_video_resolution = 1;
+const int DEFAULT_RESOLUTION = 7; // 1280x720
+int Game_video_resolution = DEFAULT_RESOLUTION;
 
 tDetailSettings Detail_settings;
 int Default_detail_level = DETAIL_LEVEL_MED;
@@ -464,7 +489,8 @@ void ConfigSetDetailLevel(int level) {
 
 #define IDV_GAMMAAPPLY 5
 #define IDV_AUTOGAMMA 6
-
+#define IDV_CHANGE_RES_WINDOW 10
+#define UID_RESOLUTION 110
 
 static void gamma_callback(newuiTiledWindow *wnd, void *data) {
   int bm_handle = *((int *)data);
@@ -631,7 +657,6 @@ static void config_gamma() {
   }
 }
 
-
 //////////////////////////////////////////////////////////////////
 // VIDEO MENU
 //
@@ -642,25 +667,15 @@ struct video_menu {
   bool *mipmapping;
   bool *vsync;
 
-  int *bitdepth;   // bitdepths
-  int *resolution; // all resolutions
+  int *bitdepth; // bitdepths
 
   // sets the menu up.
   newuiSheet *setup(newuiMenu *menu) {
-    int iTemp;
     sheet = menu->AddOption(IDV_VCONFIG, TXT_OPTVIDEO, NEWUIMENU_MEDIUM);
 
     // video resolution
-    iTemp = Game_video_resolution;
     sheet->NewGroup(TXT_RESOLUTION, 0, 0);
-    resolution = sheet->AddFirstLongRadioButton("512x384");
-    sheet->AddLongRadioButton("640x480");
-    sheet->AddLongRadioButton("800x600");
-    sheet->AddLongRadioButton("960x720");
-    sheet->AddLongRadioButton("1024x768");
-    sheet->AddLongRadioButton("1280x960");
-    sheet->AddLongRadioButton("1600x1200");
-    *resolution = iTemp;
+    sheet->AddLongButton("Change resolution", IDV_CHANGEWINDOW);
 
 #if !defined(POSIX)
     // renderer bit depth
@@ -685,11 +700,11 @@ struct video_menu {
     }
 #endif
     // video settings
-    sheet->NewGroup(TXT_TOGGLES, 0, 120);
+    sheet->NewGroup(TXT_TOGGLES, 0, 50);
     filtering = sheet->AddLongCheckBox(TXT_BILINEAR, (Render_preferred_state.filtering != 0));
     mipmapping = sheet->AddLongCheckBox(TXT_MIPMAPPING, (Render_preferred_state.mipping != 0));
 
-    sheet->NewGroup(TXT_MONITOR, 0, 180);
+    sheet->NewGroup(TXT_MONITOR, 0, 110);
     vsync = sheet->AddLongCheckBox(TXT_CFG_VSYNCENABLED, (Render_preferred_state.vsync_on != 0));
 
     sheet->AddText("");
@@ -715,35 +730,67 @@ struct video_menu {
       rend_SetPreferredState(&Render_preferred_state);
     }
 
-    if ((*resolution) != Game_video_resolution) {
-      // if in game, do resolution change.
-      int temp_w, temp_h;
-      int old_sm = GetScreenMode();
-
-      Game_video_resolution = *resolution;
-
-      if (old_sm == SM_GAME) {
-        SetScreenMode(SM_NULL);
-        SetScreenMode(old_sm, true);
-      }
-      temp_w = Video_res_list[Game_video_resolution].width;
-      temp_h = Video_res_list[Game_video_resolution].height;
-      Current_pilot.set_hud_data(NULL, NULL, NULL, &temp_w, &temp_h);
-    }
-
     sheet = NULL;
   };
 
   // process
   void process(int res) {
     switch (res) {
+    case IDV_CHANGE_RES_WINDOW: {
+      // Resolution configuration window
+      newuiTiledWindow menu;
+      newuiSheet *select_sheet;
+      newuiListBox *resolution_list;
+
+      menu.Create("Resolution", 0, 0, 300, 400);
+      select_sheet = menu.GetSheet();
+      select_sheet->NewGroup(NULL, 10, 0);
+      resolution_list = select_sheet->AddListBox(208, 257, UID_RESOLUTION, UILB_NOSORT);
+      select_sheet->NewGroup(NULL, 100, 300, NEWUI_ALIGN_HORIZ);
+      select_sheet->AddButton(TXT_OK, UID_OK);
+      select_sheet->AddButton(TXT_CANCEL, UID_CANCEL);
+
+      for (auto &resolution : Video_res_list) {
+        resolution_list->AddItem(resolution.getName().c_str());
+      }
+
+      menu.Open();
+
+      resolution_list->SetCurrentIndex(Game_video_resolution);
+
+      int res;
+      do {
+        res = menu.DoUI();
+      } while (res != UID_OK && res != UID_CANCEL);
+
+      if (res == UID_OK) {
+        int newindex = resolution_list->GetCurrentIndex();
+        if (newindex < Video_res_list.size()) {
+          Game_video_resolution = newindex;
+          int temp_w, temp_h;
+          int old_sm = GetScreenMode();
+
+          if (old_sm == SM_GAME) {
+            SetScreenMode(SM_NULL);
+            SetScreenMode(old_sm, true);
+          }
+
+          temp_w = Video_res_list[Game_video_resolution].width;
+          temp_h = Video_res_list[Game_video_resolution].height;
+          Current_pilot.set_hud_data(NULL, NULL, NULL, &temp_w, &temp_h);
+        }
+      }
+
+      menu.Close();
+      menu.Destroy();
+      break;
+    }
     case IDV_AUTOGAMMA:
       config_gamma();
       break;
     }
   };
 };
-
 
 //////////////////////////////////////////////////////////////////
 // SOUND MENU
@@ -755,7 +802,7 @@ struct sound_menu {
 
   int16_t *fxvolume, *musicvolume; // volume sliders
   int16_t *fxquantity;             // sound fx quantity limit
-  int *fxquality;                // sfx quality low/high
+  int *fxquality;                  // sfx quality low/high
 
   int16_t old_fxquantity;
 
@@ -920,7 +967,6 @@ struct sound_menu {
   };
 };
 
-
 //////////////////////////////////////////////////////////////////
 // GENERAL SETTINGS (TOGGLES) MENU
 //
@@ -1035,7 +1081,6 @@ struct toggles_menu {
     }
   };
 };
-
 
 //////////////////////////////////////////////////////////////////
 //  HUD CONFIG MENU
@@ -1158,7 +1203,6 @@ struct hud_menu {
   };
 };
 
-
 //////////////////////////////////////////////////////////////////
 // DETAILS MENU
 //
@@ -1171,7 +1215,7 @@ struct details_menu {
   bool *specmap, *headlight, *mirror, // check boxes
       *dynamic, *fog, *coronas, *procedurals, *powerup_halo, *scorches, *weapon_coronas;
   int16_t *pixel_err, // 0-27 (1-28)
-      *rend_dist;   // 0-120 (80-200)
+      *rend_dist;     // 0-120 (80-200)
 
   int *texture_quality;
 

Descent3/config.h
@@ -118,6 +118,8 @@
 #define CONFIG_H_
 
 #include <cstdint>
+#include <sstream>
+#include <vector>
 
 // Main menu configuration functions
 
@@ -133,20 +135,20 @@ extern tGameToggles Game_toggles;
 // this list should match the list in config.cpp to work.
 #define N_SUPPORTED_VIDRES 8
 
-#define RES_512X384 0
-#define RES_640X480 1
-#define RES_800X600 2
-#define RES_960X720 3
-#define RES_1024X768 4
-#define RES_1280X960 5
-#define RES_1600X1200 6
 // stored resolution list and desired game resolution
 struct tVideoResolution {
   uint16_t width;
   uint16_t height;
+
+  std::string getName() const {
+    std::stringstream ss;
+    ss << this->width << "x" << this->height;
+    return ss.str();
+  }
 };
 
-extern tVideoResolution Video_res_list[];
+extern std::vector<tVideoResolution> Video_res_list;
+extern const int DEFAULT_RESOLUTION;
 extern int Game_video_resolution;
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

Descent3/game.cpp
@@ -982,7 +982,7 @@ void SetScreenMode(int sm, bool force_res_change) {
         // We're using the default, so change some values for the menus
         rend_initted = 1;
         LOG_INFO << "Changing menu settings to default!";
-        Game_video_resolution = RES_640X480;
+        Game_video_resolution = DEFAULT_RESOLUTION;
         Render_preferred_state.bit_depth = 32;
         scr_width = 640;
         scr_height = 480;
@@ -1000,7 +1000,7 @@ void SetScreenMode(int sm, bool force_res_change) {
           // We're using the default, so change some values for the menus
           rend_initted = 1;
           LOG_INFO << "Changing menu settings to default!";
-          Game_video_resolution = RES_640X480;
+          Game_video_resolution = DEFAULT_RESOLUTION;
           Render_preferred_state.bit_depth = 32;
           scr_width = 640;
           scr_height = 480;

Descent3/init.cpp
@@ -1231,7 +1231,7 @@ void LoadGameSettings() {
   D3Use_force_feedback = true;
   D3Force_gain = 1.0f;
   D3Force_auto_center = true;
-  Game_video_resolution = RES_640X480;
+  Game_video_resolution = DEFAULT_RESOLUTION;
   PlayPowerupVoice = true;
   PlayVoices = true;
   Sound_mixer = SOUND_MIXER_SOFTWARE_16;
@@ -1361,13 +1361,18 @@ void LoadGameSettings() {
   ConfigSetDetailLevel(level);
   int widtharg = FindArg("-Width");
   int heightarg = FindArg("-Height");
-  if (widtharg) {
-    Video_res_list[N_SUPPORTED_VIDRES - 1].width = atoi(GameArgs[widtharg + 1]);
-    Game_video_resolution = N_SUPPORTED_VIDRES - 1;
+
+  if (widtharg && !heightarg) {
+    LOG_ERROR << "Specify '-Height' argument when setting '-Width'";
   }
-  if (heightarg) {
-    Video_res_list[N_SUPPORTED_VIDRES - 1].height = atoi(GameArgs[heightarg + 1]);
-    Game_video_resolution = N_SUPPORTED_VIDRES - 1;
+  if (heightarg && !widtharg) {
+    LOG_ERROR << "Specify '-Width' argument when setting '-Height'";
+  }
+
+  if (widtharg && heightarg) {
+    Video_res_list.emplace_back(tVideoResolution{static_cast<unsigned short>(atoi(GameArgs[widtharg + 1])),
+                                                 static_cast<unsigned short>(atoi(GameArgs[heightarg + 1]))});
+    Game_video_resolution = Video_res_list.size() - 1;
   }
 
   // Motion blur
@@ -1419,7 +1424,8 @@ void InitIOSystems(bool editor) {
   while (0 != (additionaldirarg = FindArg("-additionaldir", additionaldirarg))) {
     const auto dir_to_add = GetArg(additionaldirarg + 1);
     if (dir_to_add == NULL) {
-      LOG_WARNING << "-additionaldir was at the end of the argument list. It should never be at the end of the argument list.";
+      LOG_WARNING
+          << "-additionaldir was at the end of the argument list. It should never be at the end of the argument list.";
       break;
     } else {
       cf_AddBaseDirectory(std::filesystem::path(dir_to_add));
@@ -1909,7 +1915,7 @@ void InitD3Systems2(bool editor) {
 
   // the remaining sound system
   InitVoices();
-  InitD3Music(FindArg("-nomusic")  || FindArg("-nosound") ? false : true);
+  InitD3Music(FindArg("-nomusic") || FindArg("-nosound") ? false : true);
   InitAmbientSoundSystem();
 
   InitGameSystems(editor);
@@ -1957,7 +1963,7 @@ void SetupTempDirectory(void) {
     std::error_code ec;
     std::filesystem::path tempPath = std::filesystem::temp_directory_path(ec);
     if (ec) {
-      Error("Could not find temporary directory: \"%s\"", ec.message().c_str() );
+      Error("Could not find temporary directory: \"%s\"", ec.message().c_str());
       exit(1);
     }
     Descent3_temp_directory = tempPath / "Descent3" / "cache";
@@ -2032,8 +2038,8 @@ void DeleteTempFiles() {
   ddio_DoForeachFile(Descent3_temp_directory, std::regex("d3[smocti].+\\.tmp"), [](const std::filesystem::path &path) {
     std::error_code ec;
     std::filesystem::remove(path, ec);
-    LOG_WARNING_IF(ec).printf("Unable to remove temporary file %s: %s\n",
-                              path.u8string().c_str(), ec.message().c_str());
+    LOG_WARNING_IF(ec).printf("Unable to remove temporary file %s: %s\n", path.u8string().c_str(),
+                              ec.message().c_str());
   });
 }
 

Descent3/huddisplay.cpp
@@ -963,7 +963,6 @@ void RenderHUDScore(tHUDItem *item) {
   snprintf(buf, sizeof(buf), "%s: %d ", TXT_SCORE, Players[Player_num].score);
 
   win_w = (Max_window_w - Game_window_w) * (Hud_aspect_x);
-  //	if (Game_video_resolution==RES_512X384) { win_w = win_w + 10; }
 
   int w = RenderHUDGetTextLineWidth(buf); // * win_w)/(Game_window_w);
   RenderHUDText(item->color, HUD_ALPHA, 0, item->x - w - win_w, item->y, buf);

commit 0f95a9860ef18c69eb8ee3d666e5f75fc1c6d955

Fix a case of static initialization order debacle
When descent3 is built with g++ -flto=auto, there is a crash at startup:

```
Program received signal SIGSEGV, Segmentation fault.
0x00005555557b3b20 in pilot::initialize (
    this=this@entry=0x555555fbd480 <Current_pilot>)
    at Descent3/pilot_class.cpp:208
208       game_window_w = Video_res_list[Current_video_resolution_id].width;
Missing separate debuginfos, use: zypper install libcpp-httplib0_20-debuginfo-0.20.1-2.1.x86_64 libSDL3-0-debuginfo-3.2.16-1.1.x86_64 libzstd1-x86-64-v3-debuginfo-1.5.7-3.1.x86_64
(gdb) p Current_video_resolution_id
$1 = 0
(gdb) p Video_res_list
$2 = std::vector of length 0, capacity 0

(gdb) bt
f0  pilot::initialize (this=this@entry=0x555555fbd480 <Current_pilot>) at Descent3/pilot_class.cpp:208
f1  pilot::pilot (this=<optimized out>, this=<optimized out>) at Descent3/pilot_class.cpp:177
f2  _sub_I_65535_0.0 ()
f3  call_init (argc=1, argv=0x7fffffffdb68, env=<optimized out>) at ../csu/libc-start.c:145
f4  __libc_start_main_impl
f5  _start () at ../sysdeps/x86_64/start.S:115
```

When ASAN/UBSAN is enabled in conjunction with LTO:

```
/usr/include/c++/14/bits/stl_vector.h:1144:34: runtime error: reference binding to null pointer of type 'struct value_type'
Descent3/pilot_class.cpp:208:63: runtime error: member access within null pointer of type 'struct value_type'
AddressSanitizer:DEADLYSIGNAL
=================================================================
==58724==ERROR: AddressSanitizer: SEGV on unknown address 0x000000000000
==58724==The signal is caused by a READ memory access.
==58724==Hint: address points to the zero page.
    f0 pilot::initialize() Descent3/pilot_class.cpp:208
    f1 pilot::pilot() Descent3/pilot_class.cpp:177
    f2 _sub_I_65535_0.0 (b/build/Descent3+0x52ece3)
    f3 call_init ../csu/libc-start.c:145
    f4 __libc_start_main_impl ../csu/libc-start.c:347
    f5 _start ../sysdeps/x86_64/start.S:115
```

``_sub_I_65535_0.0`` is indicative of a global constructor, so we
are looking at a case of Static Initialization Order Fiasco whereby
``pilot::initialize`` runs before ``Video_res_list`` gets initialized.

Static variables declared in functions are initialized on first use,
which can help steer global initialization short of placing the
global state in a class instance of its own.

Fixes: 40c7f0dbed60aca584302aaff0b31732bf8c4c4f

Descent3/config.cpp
@@ -314,31 +314,23 @@
 
 #define STAT_SCORE STAT_TIMER
 
-// This list is only used if `ConfigureDisplayResolutions` fails
-std::vector<tVideoResolution> Video_res_list = {{512, 384},
-                                                {640, 480},
-                                                {800, 600},
-                                                {960, 720},
-                                                {1024, 768},
-                                                {1280, 960},
-                                                {1600, 1200},
-                                                // 16:9
-                                                {1280, 720},
-                                                {1366, 768},
-                                                {1368, 768},
-                                                {1680, 1050},
-                                                {1920, 1080},
-                                                {2560, 1440},
-                                                {3840, 2160},
-                                                // 16:10
-                                                {1280, 800},
-                                                {1920, 1200},
-                                                {2560, 1600},
-                                                // Ultrawide
-                                                {2560, 1080},
-                                                {2880, 1200},
-                                                {3440, 1440},
-                                                {3840, 1600}};
+std::vector<tVideoResolution> &Video_res_list()
+{
+	// This list is only used if `ConfigureDisplayResolutions` fails
+	static std::vector<tVideoResolution> list = {
+		// 4:3
+		{512, 384}, {640, 480}, {800, 600}, {960, 720}, {1024, 768},
+		{1280, 960}, {1600, 1200},
+		// 16:9
+		{1280, 720}, {1366, 768}, {1368, 768}, {1680, 1050},
+		{1920, 1080}, {2560, 1440}, {3840, 2160},
+		// 16:10
+		{1280, 800}, {1920, 1200}, {2560, 1600},
+		// Ultrawide
+		{2560, 1080}, {2880, 1200}, {3440, 1440}, {3840, 1600}
+	};
+	return list;
+}
 
 int Default_resolution_id = 7; // 1280x720 in the default list
 int Current_video_resolution_id = Default_resolution_id;
@@ -417,15 +409,16 @@ void ConfigureDisplayResolutions() {
   if (resolutions_vec.empty()) {
     return;
   }
-  Video_res_list = std::move(resolutions_vec);
+  auto &vres = Video_res_list();
+  vres = std::move(resolutions_vec);
   SDL_free(displays);
 
   // Find the index of the current screen resolution in the list
-  auto current_res_id = std::find(Video_res_list.begin(), Video_res_list.end(), current_resolution);
-  if (current_res_id != Video_res_list.end()) {
-    Default_resolution_id = static_cast<int>(current_res_id - Video_res_list.begin());
+  auto current_res_id = std::find(vres.cbegin(), vres.cend(), current_resolution);
+  if (current_res_id != vres.cend()) {
+    Default_resolution_id = static_cast<int>(current_res_id - vres.cbegin());
   } else {
-    Default_resolution_id = Video_res_list.size() - 1; // default to the highest supported resolution
+    Default_resolution_id = vres.size() - 1; // default to the highest supported resolution
   }
 
   int tmp;
@@ -434,8 +427,8 @@ void ConfigureDisplayResolutions() {
     Current_video_resolution_id = Default_resolution_id;
   }
 
-  LOG_DEBUG << "Resolution configured to w=" << Video_res_list[Current_video_resolution_id].width
-            << "h=" << Video_res_list[Current_video_resolution_id].height << " (id " << Current_video_resolution_id
+  LOG_DEBUG << "Resolution configured to w=" << vres[Current_video_resolution_id].width
+            << "h=" << vres[Current_video_resolution_id].height << " (id " << Current_video_resolution_id
             << ")";
 }
 
@@ -782,7 +775,8 @@ struct video_menu {
 
     // video resolution
     sheet->NewGroup(TXT_RESOLUTION, 0, 0);
-    std::string res = Video_res_list[Current_video_resolution_id].getName();
+    auto &vres = Video_res_list();
+    std::string res = vres[Current_video_resolution_id].getName();
     auto alloc_size = std::max(res.size() + 1, static_cast<size_t>(15));
     resolution_string = sheet->AddChangeableText(alloc_size);
     snprintf(resolution_string, alloc_size, res.c_str());
@@ -856,8 +850,9 @@ struct video_menu {
       Render_preferred_state.bit_depth = Render_preferred_bitdepth;
       rend_SetPreferredState(&Render_preferred_state, true);
 
-      int temp_w = Video_res_list[Current_video_resolution_id].width;
-      int temp_h = Video_res_list[Current_video_resolution_id].height;
+      auto &vres = Video_res_list();
+      int temp_w = vres[Current_video_resolution_id].width;
+      int temp_h = vres[Current_video_resolution_id].height;
       Current_pilot.set_hud_data(NULL, NULL, NULL, &temp_w, &temp_h);
     }
 
@@ -886,7 +881,8 @@ struct video_menu {
       select_sheet->AddButton(TXT_OK, UID_OK);
       select_sheet->AddButton(TXT_CANCEL, UID_CANCEL);
 
-      for (auto &resolution : Video_res_list) {
+      auto &vres = Video_res_list();
+      for (auto &resolution : vres) {
         resolution_list->AddItem(resolution.getName().c_str());
       }
 
@@ -901,10 +897,10 @@ struct video_menu {
 
       if (res == UID_OK) {
         int newindex = resolution_list->GetCurrentIndex();
-        if (static_cast<size_t>(newindex) < Video_res_list.size() && Current_video_resolution_id != newindex) {
+        if (static_cast<size_t>(newindex) < vres.size() && Current_video_resolution_id != newindex) {
           resolution_changed = true;
           Current_video_resolution_id = newindex;
-          std::string res = Video_res_list[Current_video_resolution_id].getName();
+          std::string res = vres[Current_video_resolution_id].getName();
           snprintf(resolution_string, res.size() + 1, res.c_str());
         }
       }

Descent3/config.h
@@ -155,7 +155,7 @@ struct tVideoResolution
   }
 };
 
-extern std::vector<tVideoResolution> Video_res_list;
+extern std::vector<tVideoResolution> &Video_res_list();
 extern int Default_resolution_id;
 extern int Current_video_resolution_id;
 extern int Display_id;

Descent3/game.cpp
@@ -937,8 +937,9 @@ void SetScreenMode(int sm, bool force_res_change) {
     int scr_width, scr_height, scr_bitdepth;
 
     if (sm == SM_GAME) {
-      scr_width = Video_res_list[Current_video_resolution_id].width;
-      scr_height = Video_res_list[Current_video_resolution_id].height;
+      auto &vres = Video_res_list();
+      scr_width = vres[Current_video_resolution_id].width;
+      scr_height = vres[Current_video_resolution_id].height;
       scr_bitdepth = Render_preferred_bitdepth;
     } else {
       scr_width = FIXED_SCREEN_WIDTH;

Descent3/init.cpp
@@ -1305,7 +1305,7 @@ void LoadGameSettings() {
   Database->read("Specmapping", &Detail_settings.Specular_lighting);
   Database->read("RS_bitdepth", &Render_preferred_bitdepth, sizeof(Render_preferred_bitdepth));
   Database->read_int("RS_resolution", &tempint);
-  if (tempint >= 0 && tempint < std::size(Video_res_list))
+  if (tempint >= 0 && tempint < Video_res_list().size())
     Current_video_resolution_id = tempint;
   else
     LOG_WARNING << "Game settings contain a display resolution index that is out of bounds. Starting with default resolution.";

Descent3/pilot_class.cpp
@@ -205,8 +205,15 @@ void pilot::initialize(void) {
   hud_mode = (uint8_t)HUD_COCKPIT;
   hud_stat = 0;
   hud_graphical_stat = STAT_STANDARD;
-  game_window_w = Video_res_list[Current_video_resolution_id].width;
-  game_window_h = Video_res_list[Current_video_resolution_id].height;
+  auto &vres = Video_res_list();
+  if (Current_video_resolution_id >= 0 &&
+      static_cast<size_t>(Current_video_resolution_id) < vres.size()) {
+		game_window_w = vres[Current_video_resolution_id].width;
+		game_window_h = vres[Current_video_resolution_id].height;
+  } else {
+		game_window_w = 0;
+		game_window_h = 0;
+  }
   num_missions_flown = 0;
   mission_data = NULL;
   mouselook_control = false;

renderer/HardwareOpenGL.cpp
@@ -348,8 +348,9 @@ void opengl_SetDefaults() {
 extern renderer_preferred_state Render_preferred_state;
 
 int opengl_Setup(oeApplication *app, const int *width, const int *height) {
-  int winw = Video_res_list[Current_video_resolution_id].width;
-  int winh = Video_res_list[Current_video_resolution_id].height;
+  auto &vres = Video_res_list();
+  int winw = vres[Current_video_resolution_id].width;
+  int winh = vres[Current_video_resolution_id].height;
 
   SDL_ClearError();
   if (!SDL_WasInit(SDL_INIT_VIDEO)) {