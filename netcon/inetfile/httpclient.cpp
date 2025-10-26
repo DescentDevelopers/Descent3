d7e7ff129640d373bbef2b06cf776d93f6d9425f
Jon Daniel <joneqdaniel@gmail.com>
2025-10-26 02:35:23 +0000

    httplib version bump compatible type rename
    - use HttpClient::Progress instead of httplib::[Download]Progress

--- netcon/inetfile/httpclient.cpp
+++ netcon/inetfile/httpclient.cpp
@@ -30,7 +30,7 @@ httplib::Result HttpClient::Get(const std::string &URIPath) {
     return m_client->Get(URIPath);
 }
 
-httplib::Result HttpClient::Get(const std::string &URIPath, const httplib::Progress &progress) {
+httplib::Result HttpClient::Get(const std::string &URIPath, const HttpClient::Progress &progress) {
   return m_client->Get(URIPath, progress);
 }
 
@@ -39,7 +39,7 @@ httplib::Result HttpClient::Get(const std::string &URIPath, const httplib::Conte
 }
 
 httplib::Result HttpClient::Get(const std::string &URIPath, const httplib::ContentReceiver &content_receiver,
-                    const httplib::Progress &progress) {
+                    const HttpClient::Progress &progress) {
   return m_client->Get(URIPath, content_receiver, progress);
 }
 
